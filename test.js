3ebf515a290c903160f220c09234dc0d5c0f1702
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2021-07-08 14:20:58 +0200

    Lesson 13 else if statements added to test.js file

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-alert("Hello world");
+var myAge=29;
+
+if (myAge > 30){
+    document.write("you are over 20");
+
+
+} else if (myAge > 20){
+        document.write("you are over 20");
+    
+
+ } else if (myAge > 10){
+        document.write("you are over 10");
+    
+
+ } else {
+        document.write("you are not over 10");
+    }
